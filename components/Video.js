d1bf6d9898a33680e35857790cde391a30e6a016
Javar Alexander <Javar.Alexander@gmail.com>
2020-12-26 13:36:41 -0500

    broken

--- components/Video.js
+++ components/Video.js
@@ -1,31 +1,88 @@
 import { Grid } from "@material-ui/core";
 import { useEffect, useRef, useState } from "react";
 
+function loadPeerPromise() {
+  return import("peerjs").then((mod) => mod.default);
+}
+
 export default function Video({
   isSelf = false,
   name = "Pano",
   src = "myVideo",
+  socket,
+  roomId,
 }) {
   const videoRef = useRef();
+  const [connectedUsers, setConnectedUsers] = useState([]);
+  const [peer, setPeer] = useState(null);
+  const [myStream, setMyStream] = useState({});
+
+  useEffect(() => {
+    loadPeerPromise().then((Peer) => {
+      const myPeer = new Peer(undefined, {
+        host: "/",
+        port: "3001",
+      });
+      myPeer.on("open", (id) => {
+        socket.emit("join-room", roomId, id);
+      });
+      setPeer(myPeer);
+    });
+  }, []);
 
   if (isSelf) {
     useEffect(() => {
-      navigator.mediaDevices
-        .getUserMedia({
-          audio: true,
-          video: true,
-        })
-        .then((stream) => {
-          videoRef.current.srcObject = stream;
-          videoRef.current.play();
-          videoRef.current.muted = true;
-        })
-        .catch(() => {
-          console.warn("Failed to get my video");
-        });
-    }, []);
+      if (peer) {
+        navigator.mediaDevices
+          .getUserMedia({
+            audio: true,
+            video: true,
+          })
+          .then((stream) => {
+            videoRef.current.srcObject = stream;
+            videoRef.current.play();
+            videoRef.current.muted = true;
+
+            setMyStream(stream);
+
+            socket.on("user-connected", (userId) => {
+              connectToNewUser(userId, stream);
+            });
+          })
+          .catch((e) => {
+            console.log(e);
+            console.warn("Failed to get my video");
+          });
+      }
+    }, [peer]);
   }
 
+  useEffect(() => {
+    if (peer) {
+      peer.on("call", (call) => {
+        call.answer(myStream);
+        call.on("stream", (userVideoStream) => {
+          console.log("userVidstream", userVideoStream);
+          setConnectedUsers([...connectedUsers, userVideoStream]);
+        });
+      });
+    }
+  });
+
+  const connectToNewUser = (userId, stream) => {
+    if (peer) {
+      const call = peer.call(userId, stream);
+      call.on("stream", (userVideoStream) => {
+        console.log("userVidstream", userVideoStream);
+        setConnectedUsers([...connectedUsers, userVideoStream]);
+      });
+
+      call.on("close", (...rest) => {
+        console.log("closed", rest);
+      });
+    }
+  };
+  console.log("connected users", connectedUsers);
   return (
     <Grid container alignItems="center" justify="center">
       <Grid item>
--- pages/room/[roomId].js
+++ pages/room/[roomId].js
@@ -5,40 +5,18 @@ import { useEffect, useState } from "react";
 import { SocketPath } from "util/Sockets";
 import io from "socket.io-client";
 
-function loadPeerPromise() {
-  return import("peerjs").then((mod) => mod.default);
-}
-
 export const Room = function (props) {
-  const [peer, setPeer] = useState(null);
-
   const router = useRouter();
   const { roomId } = router.query;
 
   const socket = io(SocketPath.sockets);
-  useEffect(() => {
-    loadPeerPromise().then((Peer) => {
-      const myPeer = new Peer(undefined, {
-        host: "/",
-        port: "3001",
-      });
-      myPeer.on("open", (id) => {
-        socket.emit("join-room", roomId, id);
-      });
-      setPeer(myPeer);
-    });
-  }, []);
-
-  socket.on("user-connected", (data) => {
-    console.log("user connected", data);
-  });
 
   return (
     <Paper>
       <Grid container spacing={3}>
         <Grid item>Room: {roomId}</Grid>
         <Grid item xs={12}>
-          <Video peer={peer} isSelf={true} />
+          <Video socket={socket} isSelf={true} roomId={roomId} />
         </Grid>
       </Grid>
     </Paper>
