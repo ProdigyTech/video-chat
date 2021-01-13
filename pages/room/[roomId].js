5a73147581a739a4dc1a44cb2c489ac151751818
Javar Alexander <Javar.Alexander@gmail.com>
2021-01-12 22:42:58 -0500

    flipping flag back

--- pages/room/[roomId].js
+++ pages/room/[roomId].js
@@ -54,7 +54,7 @@ export const Room = function ({ roomId }) {
     loadPeerPromise().then((Peer) => {
       const peer = new Peer(undefined, {
         host: "peerjs.prodigytech.us",
-        secure: false,
+        secure: true,
         path: "/",
       });
 
